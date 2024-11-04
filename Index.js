12754ac198f7c62afedeb12a94eaa8771b531c93
Iman-Fatima0 <iman.f8094@gmail.com>
2024-11-04 12:40:54 +0500

    commit

--- Index.js
+++ Index.js
@@ -4,7 +4,7 @@ const express = require('express');
 const app = express();
 const port = 8000;
 
-fs.appendFile("Readm.md", "");
+fs.appendFile("Hello.txt", "Hello world!");
 
 
 app.get('/api/rooms', (req, res) => {
