64c534994274344dd09f8bfedd5fc32d0c0c3ea0
Iman-Fatima0 <iman.f8094@gmail.com>
2024-11-08 21:42:53 +0500

    fs edit

--- Index.js
+++ Index.js
@@ -2,32 +2,44 @@ const rooms = require('./Data');
 const fs = require('fs');
 const express = require('express');
 const app = express();
-const port = 8000;
-
-fs.appendFile("Hello.txt", "Hello world!");
-
+const port = 5600;
+// const data=data.rooms;
+fs.appendFile("Hello.txt", "I am creating a file with js code that conatiansAPI creation through get method !",(err)=>
+{
+    if(err)
+    {
+        console.error("Failed to write to file");
+    }
+    else{
+        console.log("Message written to Hello.txt");
+        console.log('Server is running on port 5600');  // Replace with your desired port number. 5600 is just an example. You can choose any port number.
+    }
+});
+app.get('/', (req, res) =>{
+res.send('You are currently getting the data for rooms!'); 
+})
 
 app.get('/api/rooms', (req, res) => {
     res.json(rooms);
 });
 
 app.get('/api/rooms/available', (req, res) => {
-    const available = rooms.filter(room => room.available);
+    const available = rooms.filter((room) => room.available);
     res.json(available);
 });
 
 app.get('/api/rooms/single', (req, res) => {
-    const single = rooms.filter(room => room.type === 'single');
+    const single = rooms.filter((room) => room.type == 'single');
     res.json(single);
 });
 
 app.get('/api/rooms/double', (req, res) => {
-    const double = rooms.filter(room => room.type === 'double');
+    const double = rooms.filter((room) => room.type == 'double');
     res.json(double);
 });
 
 app.get('/api/rooms/price/under-150', (req, res) => {
-    const under150 = rooms.filter(room => room.price < 150);
+    const under150 = rooms.filter((room) => room.price < 150);
     res.json(under150);
 });
 
